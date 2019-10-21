f03df9f099c8a05be9e943f025d05aa3b58a1e28
OlyaHlova <olyastef@gmail.com>
2019-10-21 13:38:33 +0000

    Pakaz

--- js/clock.js
+++ js/clock.js
@@ -1,10 +1,10 @@
 function getTimeRemaining(endtime) {
-    var t = Date.parse(endtime) - Date.parse(new Date());
-    var seconds = Math.floor((t / 1000) % 60);
-    var minutes = Math.floor((t / 1000 / 60) % 60);
-    var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
-    var days = Math.floor(t / (1000 * 60 * 60 * 24));
-    var years = Math.floor(t / (1000 * 60 * 60 * 24 * 365));
+    let t = Date.parse(endtime) - Date.parse(new Date());
+    let seconds = Math.floor((t / 1000) % 60);
+    let minutes = Math.floor((t / 1000 / 60) % 60);
+    let hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+    let days = Math.floor(t / (1000 * 60 * 60 * 24));
+    let years = Math.floor(t / (1000 * 60 * 60 * 24 * 365.25));
     return {
       'total': t,
       'years': years,
@@ -14,32 +14,32 @@ function getTimeRemaining(endtime) {
       'seconds': seconds
     };
   }
-  
+
   function initializeClock(id, endtime) {
-    var clock = document.getElementById(id);
-    var yearsSpan = clock.querySelector('.years');
-    var daysSpan = clock.querySelector('.days');
-    var hoursSpan = clock.querySelector('.hours');
-    var minutesSpan = clock.querySelector('.minutes');
-    var secondsSpan = clock.querySelector('.seconds');
-  
+    let clock = document.getElementById(id);
+    let yearsSpan = clock.querySelector('.years');
+    let daysSpan = clock.querySelector('.days');
+    let hoursSpan = clock.querySelector('.hours');
+    let minutesSpan = clock.querySelector('.minutes');
+    let secondsSpan = clock.querySelector('.seconds');
+
     function updateClock() {
-      var t = getTimeRemaining(endtime);
-  
+      let t = getTimeRemaining(endtime);
+
       yearsSpan.innerHTML = t.years;
       daysSpan.innerHTML = ('0' + t.years).slice(-2);
       hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
       minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
       secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
-  
+
       if (t.total <= 0) {
         clearInterval(timeinterval);
       }
     }
-  
+
     updateClock();
-    var timeinterval = setInterval(updateClock, 1000);
+    let timeinterval = setInterval(updateClock, 1000);
   }
-  
-  var deadline="January 01 2218 00:00:00 GMT+0300"; //for Ukraine
+
+  let deadline="January 01 2502 00:00:00 GMT+0300"; //for Ukraine
   initializeClock('countdown', deadline);
